5213eca5f8a8c5b15c7742e65c326b4a57c5a9ba
toleukhannov <toleuhanovbatyrbek@gmail.com>
2024-05-06 23:37:28 +0500

    book.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/batyrbek/pkg/routes"
+)
+
 func main(){
-	
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
