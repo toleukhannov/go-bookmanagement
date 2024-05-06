d85961afed56e7f70819fbdb95c050f0907ed62b
toleukhannov <toleuhanovbatyrbek@gmail.com>
2024-05-06 23:23:21 +0500

    app.go

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -3,3 +3,18 @@ package controllers
 func CreateBook(){
 	
 }
+func GetBook(){
+
+}
+
+func GetBookById(){
+
+}
+
+func UpdateBook(){
+
+}
+
+func DeleteBook(){
+	
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"github.com/batyrbek/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `gorm:""json:"author"`
+	Publication string `gorm:""json:"publication"`
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+}
